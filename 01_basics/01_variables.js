c08d7cb98007267abc7fdd4a2bb8805c71ec610b
anirban-12323 <anirbanguharoy82@gmail.com>
2025-03-14 09:26:01 +0000

    variables and datatype done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId="12345"
+let accountEmail="anirban2gmail.com"
+var accountPassword="5678"
+accountCity="kolkata"
+// accountId="987"
+// console.log(accountId);
+accountEmail="roy@gmail.com"
+accountPassword="454545"
+accountCity="siliguri"
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";// treat all js code as newer version
+// alert(3+3) we are using node.js not browser
+//number=>2 to the power 53
+// bigint
+//null=>stand alone value
+// undifined
+console.log(typeof"Anirban");
+console.log(typeof null);
+console.log(typeof undefined);

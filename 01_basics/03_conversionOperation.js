507e91fe5eb71707371d606828d1779f47226b63
anirban-12323 <anirbanguharoy82@gmail.com>
2025-03-15 15:38:47 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="33aa";
+// console.log(typeof score);
+// console.log(typeof(score));
+let valueInnumber=Number(score);
+//console.log(valueInnumber);
+let isloggedin="anirban"
+let booleanisloggedin=Boolean(isloggedin);
+// console.log(booleanisloggedin);
+
+let someNumber=33
+let StringNumber=String(someNumber)
+// console.log(StringNumber)
+// console.log(typeof StringNumber);/
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+"2"+2);
+// console.log(1+2+"2");
+let x=2
+const y=x++
+console.log("x",x,"y",y);
+let a=2
+const b=++a
+console.log("a",a,"b",b);

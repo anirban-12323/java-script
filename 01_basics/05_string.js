1f858acf0f2ba1d5df37020f983a58ad85ad3158
anirban-12323 <anirbanguharoy82@gmail.com>
2025-03-15 20:32:01 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name="Anirban"
+const repocount=50
+//console.log(`hello my name is ${name} i have ${repocount}repo`);
+const gameName=new String('anirban')
+//console.log(gameName.toUpperCase());
+const newString=gameName.substring(0,4)
+//console.log(newString);
+const anotherString=gameName.slice(-7,4)
+//console.log(anotherString);
+const newstringOne="      anirban      "
+console.log(newstringOne);
+console.log(newstringOne.trim());
+const url="https:anirban:/yoyo%20honeysingh"
+console.log(url.replace('%20','--'))

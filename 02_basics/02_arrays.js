c8430dccdcedb3ca3a2b0ffdbe7638db1b3a36cb
anirban-12323 <anirbanguharoy82@gmail.com>
2025-03-16 17:22:26 +0000

    arrays part2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,19 @@
+const Marvel_heros=["Thor","Iron man","hulk","Spiderman"]
+const dc_heros=["super man","Batman","Flash"]
+//
+// Marvel_heros.push(dc_heros)
+//console.log(Marvel_heros)
+
+//console.log(Marvel_heros)
+// const all_heros=Marvel_heros.concat(dc_heros)
+// const all_heros=[...Marvel_heros,...dc_heros]
+// console.log(all_heros)
+// const anotherArray=[2,3,4,5,6,[5,3,[45,56,89,36,85,[456,859,20,[1,2,3],7963,1]],8,9]]
+// const real_arr=anotherArray.flat(Infinity)
+// console.log(real_arr)
+console.log(Array.isArray("Anirban"));
+console.log(Array.from("Anirban"));
+let score1=100
+let score2=200
+let score3=400
+console.log(Array.of(score1,score2,score3));

a3122cde69320371f3c1f95ac405071fbc472191
anirban-12323 <anirbanguharoy82@gmail.com>
2025-03-16 06:13:10 +0000

    nums_math

---
+++ 01_basics/06_number_math.js
@@ -0,0 +1,17 @@
+const balance =new Number(200)
+//console.log(balance.toString().length);
+const otherNumber=1234.565656
+//console.log(otherNumber.toFixed(2));
+//console.log(otherNumber.toPrecision(5));
+// console.log(Math.abs(-5656));
+// console.log(Math.round(4.2));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(10,20,30,40));
+// console.log(Math.max(10,20,30,40));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+const min=10
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
